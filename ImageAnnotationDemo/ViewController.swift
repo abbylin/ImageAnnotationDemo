2e8d900d4adf46d552076607e5dd41a4d08f36a5
Zhu Lin <zhu.lin@sas.com>
2016-02-04 15:32:09 +0800

    Initialize project

---
+++ ImageAnnotationDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ImageAnnotationDemo
+//
+//  Created by Abby(Zhu) Lin on 16/2/4.
+//  Copyright © 2016年 Zhu Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var annotationButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
